836cc8c7c3df527b79a464bceeaa09eeb52860d3
Julio Pinheiro <juliorenan@live.com>
2023-12-13 20:08:57 +0000

    added status

--- tests/unit/calculator.test.js
+++ tests/unit/calculator.test.js
@@ -1,4 +1,4 @@
-const calculator = require("../models/calculator");
+const calculator = require("../../models/calculator");
 
 test("add 2 plus 2 should be 4", () => {
   expect(calculator.addNumbers(2, 2)).toBe(4);

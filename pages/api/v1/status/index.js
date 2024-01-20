9b9554e3412f3dd5f8b896480fe43c8717e50c45
Julio Pinheiro <juliorenan@live.com>
2024-01-20 13:58:59 +0000

    Added more properties to the status endpoint.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,26 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  const currentConnections = await database.query(
+    "SELECT sum(numbackends) AS total FROM pg_stat_database;",
+  );
+
+  const maxConnections = await database.query(
+    "SELECT * FROM pg_settings WHERE name = 'max_connections';",
+  );
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  console.log(postgresVersion.rows[0].server_version);
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections.rows[0].setting),
+        opened_connections: parseInt(currentConnections.rows[0].total),
+        version: postgresVersion.rows[0].server_version,
+      },
+    },
   });
 }
 
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,9 +1,16 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("Status info should be returned and valid from status page", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  const { max_connections, opened_connections, version } =
+    responseBody.dependencies.database;
+
+  expect(max_connections).toBeDefined();
+  expect(max_connections).toBeGreaterThan(0);
+
+  expect(opened_connections).toBeDefined();
+  expect(opened_connections).toBeGreaterThan(-1);
+
+  expect(version).toBeDefined();
+  expect(parseInt(version)).toBeGreaterThan(0);
 });

8c61557edb38b311096d589e74a9f69d468e4fe4
Julio Pinheiro <juliorenan@live.com>
2024-07-04 03:41:11 +0000

    feat: adds wait for postgress

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,1 +1,18 @@
-console.log("🛑 Waiting for the Postgres services");
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres is Ready to use\n");
+  }
+}
+process.stdout.write("\n\n🛑 Waiting for the Postgres services");
+
+checkPostgres();
